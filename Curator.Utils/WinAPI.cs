53a4850b2089e098692c243a402880cfa440619e
James Cataldo <cataldojames@gmail.com>
2014-04-02 16:07:43 -0400

    Adding WinAPI hotkey bindings

--- Curator.Utils/WinAPI.cs
+++ Curator.Utils/WinAPI.cs
@@ -25,6 +25,10 @@ namespace Curator.Utils
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern int SendMessageTimeout(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam, uint fuFlags, uint uTimeout, out IntPtr result);
 
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
         [Flags]
         public enum AD_Apply : int
         {
